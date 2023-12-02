59f4c4af4ff7a220029621162b7f162ed92ad599
John-Paul Huver <Huverjohnpaul@gmail.com>
2023-12-02 01:25:34 -0500

    added a sources txt file

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ Created on Sat Nov 25 17:47:22 2023
 
 @authors: John-Paul Huver, Agnese, Feyi,Madeline
 """
+#JP--sys is used to manage the program and windows so that it can all be closed and exited properly
+import sys
+#JP--pygame used for windows and surfaces mostly
+import pygame
 
 
 
