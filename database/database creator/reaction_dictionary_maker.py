d4fdd99c2c2ae6cdc2307dae88013fd0d21b1999
John-Paul Huver <Huverjohnpaul@gmail.com>
2023-12-02 00:25:44 -0500

    bit better discrition still needs work

---
+++ database/database creator/reaction_dictionary_maker.py
@@ -0,0 +1,205 @@
+reactions_text = """
+C6H12 + 9O2 -> 6CO2 + 6H2O
+2H2 + O2 -> 2H2O
+2H2O2 -> 2H2O + O2
+Zn + 2HCl -> ZnCl2 + H2
+NaCl + AgNO3 -> AgCl + NaNO3
+HCl + NaOH -> NaCl + H2O
+Cu + 2AgNO3 -> Cu(NO3)2 + 2Ag
+C8H18 + 12O2 -> 8CO2 + 9H2O
+N2 + 3H2 -> 2NH3
+K + H2O -> KOH + H2
+C6H6 + 15O2 -> 6CO2 + 3H2O
+Fe2O3 + 3CO -> 2Fe + 3CO2
+KBr + Cl2 -> KCl + Br2
+CaCO3 -> CaO + CO2
+Al + 3HCl -> AlCl3 + H2
+H2SO4 + 2NaOH -> Na2SO4 + 2H2O
+Zn + 2H2SO4 -> ZnSO4 + H2
+2H2O -> 2H2 + O2
+Na2O2 + 2H2O -> 4NaOH + O2
+NaHCO3 -> Na2CO3 + CO2 + H2O
+3Mg + N2 -> Mg3N2
+H2O2 -> H2O + O2
+2H2O -> 2H2 + O2
+C2H4 + 3O2 -> 2CO2 + 2H2O
+Mg + 2HCl -> MgCl2 + H2
+KNO3 -> KNO2 + O2
+FeCl3 + 3NaOH -> Fe(OH)3 + 3NaCl
+4NH3 + 3O2 -> 2N2 + 6H2O
+Ca(OH)2 + 2HCl -> CaCl2 + 2H2O
+4Al + 3O2 -> 2Al2O3
+2C2H6 + 7O2 -> 4CO2 + 6H2O
+2H2O2 -> 2H2O + O2
+2K + 2H2O -> 2KOH + H2
+C7H16 + 11O2 -> 7CO2 + 8H2O
+P4 + 5O2 -> P4O10
+C12H22O11 + 12O2 -> 12CO2 + 11H2O
+CuO + H2 -> Cu + H2O
+2H2O -> 2H2 + O2
+H2SO4 + 2KOH -> K2SO4 + 2H2O
+C3H8 + 5O2 -> 3CO2 + 4H2O
+FeS + 2HCl -> FeCl2 + H2S
+NaOH + H3PO4 -> Na3PO4 + 3H2O
+C2H2 + 2.5O2 -> 2CO + H2O
+K2CO3 + 2HCl -> 2KCl + CO2 + H2O
+Mg(OH)2 + 2HCl -> MgCl2 + 2H2O
+C5H12 + 8O2 -> 5CO2 + 6H2O
+2H2 + Cl2 -> 2HCl
+HNO3 + Ba(OH)2 -> Ba(NO3)2 + 2H2O
+C3H6 + 4.5O2 -> 3CO2 + 3H2O
+Fe + 2H2O -> Fe(OH)2 + H2
+K2O + CO2 -> K2CO3
+Al2O3 + 6HCl -> 2AlCl3 + 3H2O
+C4H8 + 6O2 -> 4CO2 + 4H2O
+Pb(NO3)2 + 2KI -> PbI2 + 2KNO3
+C2H5OH + 3O2 -> 2CO2 + 3H2O
+CaO + H2O -> Ca(OH)2
+H2S + 2O2 -> 2H2O + SO2
+BaCl2 + Na2SO4 -> BaSO4 + 2NaCl
+C2H4O + 2O2 -> 2CO2 + 2H2O
+Ni + 2HCl -> NiCl2 + H2
+C6H14 + 19O2 -> 6CO2 + 7H2O
+Na2O + CO2 -> Na2CO3
+2H2S + 3O2 -> 2H2O + 2SO2
+Ca3(PO4)2 + 6HCl -> 2H3PO4 + 3CaCl2
+C2H4 + H2 -> C2H6
+2Na + 2H2O -> 2NaOH + H2
+C8H10 + 12O2 -> 8CO2 + 5H2O
+2H2O2 -> 2H2O + O2
+2K + 2H2O -> 2KOH + H2
+C4H10 + 13O2 -> 4CO2 + 5H2O
+CuO + H2 -> Cu + H2O
+2H2O -> 2H2 + O2
+H2SO4 + 2KOH -> K2SO4 + 2H2O
+C3H8 + 10O2 -> 3CO2 + 4H2O
+FeS + 2HCl -> FeCl2 + H2S
+NaOH + H3PO4 -> Na3PO4 + 3H2O
+C2H2 + 5O2 -> 2CO + H2O
+K2CO3 + 2HCl -> 2KCl + CO2 + H2O
+Mg(OH)2 + 2HCl -> MgCl2 + 2H2O
+C5H12 + 11O2 -> 5CO2 + 6H2O
+2H2 + Cl2 -> 2HCl
+HNO3 + Ba(OH)2 -> Ba(NO3)2 + 2H2O
+C3H6 + 9O2 -> 3CO2 + 3H2O
+Fe + 3H2O -> Fe(OH)3 + H2
+K2O + CO2 -> K2CO3
+Al2O3 + 6HCl -> 2AlCl3 + 3H2O
+C4H8 + 6O2 -> 4CO2 + 4H2O
+Pb(NO3)2 + 2KI -> PbI2 + 2KNO3
+C2H5OH + 3O2 -> 2CO2 + 3H2O
+CaO + H2O -> Ca(OH)2
+H2S + 2O2 -> 2H2O + SO2
+BaCl2 + Na2SO4 -> BaSO4 + 2NaCl
+C2H4O + 2O2 -> 2CO2 + 2H2O
+Ni + 2HCl -> NiCl2 + H2
+C6H14 + 19O2 -> 6CO2 + 7H2O
+Na2O + CO2 -> Na2CO3
+2H2S + 3O2 -> 2H2O + 2SO2
+Ca3(PO4)2 + 6HCl -> 2H3PO4 + 3CaCl2
+C2H4 + H2 -> C2H6
+2Na + 2H2O -> 2NaOH + H2
+C8H10 + 12O2 -> 8CO2 + 5H2O
+2H2O2 -> 2H2O + O2
+2K + 2H2O -> 2KOH + H2
+SO2 + O2 -> SO3
+H2 + F2 -> 2HF
+CH4 + 2O2 -> CO2 + 2H2O
+2Fe + 6HCl -> 2FeCl3 + 3H2
+3H2 + N2 -> 2NH3
+C7H16 + 11O2 -> 7CO2 + 8H2O
+H2O + Na -> NaOH + H2
+C12H22O11 + 12O2 -> 12CO2 + 11H2O
+C4H6 + 7O2 -> 4CO2 + 3H2O
+Cr2O3 + 3CO -> 2Cr + 3CO2
+KClO3 -> KCl + 3O2
+C6H5OH + 7O2 -> 6CO2 + 3H2O
+Ba(OH)2 + 2HNO3 -> Ba(NO3)2 + 2H2O
+C2H6 + 8O2 -> 2CO2 + 3H2O
+Ni(NO3)2 + 2NaOH -> Ni(OH)2 + 2NaNO3
+C2H4 + 3O2 -> 2CO2 + 2H2O
+P4 + 6Cl2 -> 4PCl3
+H2O2 -> H2O + O2
+C8H18 + 25O2 -> 8CO2 + 9H2O
+2H2 + Br2 -> 2HBr
+CuSO4 + Zn -> ZnSO4 + Cu
+C3H6O + 4O2 -> 3CO2 + 3H2O
+MnO2 + 4HCl -> MnCl2 + 2H2O + Cl2
+H2SO4 + Ca(OH)2 -> CaSO4 + 2H2O
+C5H10 + 8O2 -> 5CO2 + 5H2O
+H2O + P4 -> H3PO3 + 3H2
+C2H5OH + 2O2 -> 2CO2 + 3H2O
+Cu + 4HNO3 -> Cu(NO3)2 + 2NO2 + 2H2O
+HCl + AgNO3 -> AgCl + HNO3
+C6H14 + 19O2 -> 6CO2 + 7H2O
+2H2O + 2Na -> 2NaOH + H2
+C12H22O11 + 12O2 -> 12CO2 + 11H2O
+Mg + 2H2O -> Mg(OH)2 + H2
+2K + 2H2O -> 2KOH + H2
+SO2 + O2 -> 2SO3
+H2 + F2 -> 2HF
+CH4 + 2O2 -> CO2 + 2H2O
+2Fe + 6HCl -> 2FeCl3 + 3H2
+3H2 + N2 -> 2NH3
+C7H16 + 11O2 -> 7CO2 + 8H2O
+H2O + Na -> NaOH + H2
+C12H22O11 + 12O2 -> 12CO2 + 11H2O
+Ba(OH)2 + 2HNO3 -> Ba(NO3)2 + 2H2O
+C2H6 + 8O2 -> 2CO2 + 3H2O
+Ni(NO3)2 + 2NaOH -> Ni(OH)2 + 2NaNO3
+C2H4 + 3O2 -> 2CO2 + 2H2O
+"""
+
+def reaction_maker(reaction_text,output_filename,output_dic_name):
+    reactions = {}
+
+    # split based off of new lines of a stripped of white space entry text, split into a list
+    lines = reaction_text.strip().split('\n')
+    
+    # start line number at 0
+    line_number = 0
+    
+    # go through each line in list and extract reactants and products
+    for line in lines:
+        #split the reaction based on the arrow
+        reaction_components = line.split('->')
+   
+        #set the first and last parts to their appropriate reactents and products 
+        reactants = reaction_components[0].strip()
+        products = reaction_components[1].strip()
+       
+        # Split reactants and products into individual components
+        reactants = [r.strip() for r in reactants.split('+')]
+        products = [p.strip() for p in products.split('+')]
+
+        # Create a reaction dictionary entry
+        reaction_entry = {
+           'reactants': reactants,
+           'products': products
+           }
+        
+        # Add the reaction entry to the reactions dictionary
+        reaction_key = 'reaction' + str(line_number)
+        reactions[reaction_key] = reaction_entry
+
+        # add to line number to go to next line
+        line_number += 1
+    
+    #make the newly made dictionary into a new file
+    with open(output_filename, 'w') as output_file:
+        
+        # write a comment indicating a newly generated dictionary from this file
+        output_file.write("# Newly generated dictionary made by reaction_maker\n")
+
+        # write the dictionary name and an = sign
+        output_file.write(output_dic_name + " = {\n")
+
+        # write the dictionary using a loop going through the dictionary.items() adding each line's key and value
+        for key, value in reactions.items():
+            output_file.write("\t'" + key + "': " + str(value) + ",\n")
+        #add the ending 
+        output_file.write("}")
+    print("done file made called:"+str(output_filename))
+
+
+reaction_maker(reactions_text, "reactions_database.py", "rdict")
---
+++ main.py
@@ -0,0 +1,11 @@
+#Reactle
+#Main document that runs the main functions of the game: Reactle
+"""
+Created on Sat Nov 25 17:47:22 2023
+
+@authors: John-Paul Huver, Agnese Lasmane, Feyi Ob....,Madeline
+"""
+
+
+
+
