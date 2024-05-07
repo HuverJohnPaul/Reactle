48d52c1730842e4b63f2564bfbcf7ee5b162fd9d
John-Paul Huver <Huverjohnpaul@gmail.com>
2024-05-07 17:52:49 -0400

    Removed unfinished code now isolated to "Under-Devolpment-Updates" branch

--- unfinished/executable(unfinished)/Reactle.py
+++
@@ -1,5 +0,0 @@
-#file that contains the code to run the game on Linux, MacOS, or Windows by making a terminal appear running the main.py file
-#this file is frozen into an executable using cx_Freeze
-#this file will be converted into a .exe file using cx_Freeze so that it can be run without prior installation of Python or any modules
-#when the .exe file is run for the first time, it will in the background create a folder called "build" which will contain all the necessary files to run the game
-#the .exe file will then run the game by running the main.py file in the build folder
